c3a60da7da8c2c39e983a611fcd5f68adbb9807b
Thibaut Izard <thibaut.izard@gmail.com>
2024-11-30 15:51:28 +0100

    Init

---
+++ GymTracker/GymTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymTrackerApp.swift
+//  GymTracker
+//
+//  Created by Thibaut Izard on 30/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GymTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GymTrackerTests/GymTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GymTrackerTests.swift
+//  GymTrackerTests
+//
+//  Created by Thibaut Izard on 30/11/2024.
+//
+
+import Testing
+@testable import GymTracker
+
+struct GymTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
